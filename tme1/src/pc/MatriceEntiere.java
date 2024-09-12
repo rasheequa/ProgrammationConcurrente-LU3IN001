1712fae3ff4f6e95b142a001c2279b7c6a9e29f3
rashe <rashe@10.51.34.137>
2024-09-12 20:45:17 +0200

    ajout de commentaire

--- tme1/src/pc/MatriceEntiere.java
+++ tme1/src/pc/MatriceEntiere.java
@@ -8,82 +8,94 @@ import java.io.IOException;
 import pc.test.TaillesNonConcordantesException;
 
 public class MatriceEntiere {
-	private int [][] matrice;
+	private int [][] matrice; // matrice à deux dimensions
 	
 	public MatriceEntiere(int ligne, int colonne) {
 		
-		matrice = new int [ligne][];
-		
-		for(int i = 0; i<matrice.length;i++) {
-			matrice[i]= new int [colonne];
-			for(int j = 0; j<matrice[i].length;j++) {
-					matrice[i][j]= 0;
+		matrice = new int [ligne][]; // Crée un tableau d'entiers
+        
+		// Initialise chaque ligne de la matrice et la remplit avec des zéros
+		for(int i = 0; i<nbLignes();i++) {
+			matrice[i]= new int [colonne]; 
+			for(int j = 0; j<nbColonnes();j++) {
+				setElem(i,j,0);
 			}				
 		}
 		
 	}
 	
 	public int getElem(int lig, int col) {
-		return matrice[lig][col];
+		return matrice[lig][col]; // Elément de la matrice à une position donnée
+	    
 	}
 	
 	public void setElem(int lig, int col, int val) {
-		matrice[lig][col]=val;
+		matrice[lig][col]=val; // Attribut une valeur à un element de la matrice à une position donnée
 		
 	}
 	
 	 public int nbLignes() {
-		 return matrice.length;
+		 return matrice.length; // Retourne le nombre de lignes de la matrice
 	 }
 	 
 	 public int nbColonnes() {
-		 return matrice[0].length;
+		 return matrice[0].length; // Retourne le nombre de colonnes de la matrice
 	 }
 	 
+	// Méthode statique pour lire une matrice depuis un fichier
 	 public static MatriceEntiere parseMatrix(File fichier) throws
 	 IOException{
 		 try (BufferedReader in = new BufferedReader ( new FileReader(fichier))){
+
+			 // Lit le nombre de lignes et de colonnes depuis le fichier
 			 String nbligStr = in.readLine();
 			 int nblig = Integer.parseInt(nbligStr);
 			 int nbcol = Integer.parseInt (in.readLine());
+			 
+			 // Crée une nouvelle matrice avec ces dimensions
 			 MatriceEntiere mat = new MatriceEntiere(nblig,nbcol);
+			 
+			// Remplit la matrice en lisant les valeurs ligne par ligne
 			 for(int i = 0; i < nblig; i++) {
 				 String line = in.readLine();
 				 String[] word = line.split(" ");
 				 for(int j = 0; j<nbcol; j++) {
-					 mat.matrice[i][j] = Integer.parseInt(word[j]);
+					 mat.setElem(i,j, Integer.parseInt(word[j]));
 				 }
 			 }
 			return mat;
 		 }
-		 catch (Exception e) { throw new IOException(e);
+		 catch (Exception e) { throw new IOException(e); // Lance une exception en cas d'erreur
+         
 		 }
 		 
 	 }
 	 
+	 // Retourne une représentation de la matrice sous forme de chaîne de caractères
 	 public String toString() {
 		 String s = "";
-		 for(int[] mat : matrice) {
-			 for(int val : mat) {
-				 s+= val+" ";
+		 for(int[] mat : matrice) { // Parcours les lignes de la matrice
+			 for(int val : mat) { // Parcours les colonnes de la matrice
+				 s+= val+" "; 
 			 } 
-			 s += "\n";
+			 s += "\n"; // Retour à la ligne apres le parcours d'une colonne
 		 }
 		  
 		 return s;
 	 }
 	 
+	// Méthode equals pour comparer deux matrices
 	 public boolean equals(Object o) {
 		 if (this == o) return true;
 		 
-		 
 		 if (o instanceof MatriceEntiere) {
-			 
+			 // Vérifie si les matrices ont le même nombre de lignes
 			 if (this.nbLignes()!= ((MatriceEntiere) o).nbLignes()) return false;
 			 
-			 for(int i = 0; i<this.matrice.length;i++) {
-				 for(int j =0; j<this.matrice[i].length; j++) {
-					 if (((MatriceEntiere)o).matrice[i][j]!=this.matrice[i][j]) {
+			 // Compare chaque élément des deux matrices
+			 for(int i = 0; i<this.nbLignes();i++) {
+				 for(int j =0; j<this.nbColonnes(); j++) {
+					 if (((MatriceEntiere)o).getElem(i,j)!=this.getElem(i,j)) {
 						 return false;
 					 }
 				 }
@@ -93,47 +105,55 @@ public class MatriceEntiere {
 		 return false;
 	 }
 	 
+	// Méthode pour additionner deux matrices, lève une exception si les tailles ne correspondent pas
 	 public MatriceEntiere ajoute(MatriceEntiere m) throws TaillesNonConcordantesException{
-		 if(this.nbLignes()!= ( m.nbLignes())){
+		 if(this.nbLignes()!=( m.nbLignes())){
 	            throw new TaillesNonConcordantesException("Les dimensions ne sont pas concordantes : "+this.nbLignes()+" != "+m.nbLignes());
 	        }
-		 
+		// Crée une nouvelle matrice pour stocker le résultat de l'addition
 		 MatriceEntiere mat = new MatriceEntiere(this.nbLignes(),this.nbLignes());
 		 
-		 for(int i = 0; i<m.matrice.length;i++) {
-				for(int j = 0; j<m.matrice[i].length;j++) {
-						mat.matrice[i][j]=m.matrice[i][j]+this.matrice[i][j] ;
+		// Additionne les éléments correspondants des deux matrices
+		 for(int i = 0; i<m.nbLignes();i++) {
+				for(int j = 0; j<m.nbColonnes();j++) {
+						mat.setElem(i,j,m.getElem(i,j)+this.getElem(i,j)) ;
 				}			
 		 }
 		 return mat;
 	 }
 	 
+	// Méthode pour multiplier deux matrices, lève une exception si les tailles ne correspondent pas
 	 public MatriceEntiere produit(MatriceEntiere m) throws TaillesNonConcordantesException{
 		 
 		 if(this.nbColonnes()!= ( m.nbLignes())){
 	            throw new TaillesNonConcordantesException("Les dimensions ne sont pas concordantes : "+this.nbLignes()+" != "+m.nbColonnes());
 	        }
 		 
+		// Crée une nouvelle matrice pour stocker le résultat du produit
 		 MatriceEntiere mat = new MatriceEntiere(this.nbLignes(),this.nbLignes());
 		 int sum = 0, j=0;
-		 for(int k=0; k<m.matrice.length;k++) {
-			 for(int i = 0; i<m.matrice.length;i++) {
-				 for(j = 0; j<m.matrice.length;j++) {
-					sum += this.matrice[i][k] * m.matrice[k][j] ;
+		 
+		// Calcule le produit matriciel
+		 for(int k=0; k<m.nbLignes();k++) {
+			 for(int i = 0; i<m.nbLignes();i++) {
+				 for(j = 0; j<m.nbColonnes();j++) {
+					sum += this.getElem(i,j) * m.getElem(i,j) ;
 				 }
-			 mat.matrice[i][j]=sum;
+			 mat.setElem(i,j,sum);
 			 }
 		 }
 		 return mat;
 	 }
 	 
+	// Méthode pour obtenir la transposée d'une matrice
 	 public MatriceEntiere transposee() {
 		 
 		 MatriceEntiere mat = new MatriceEntiere(this.nbLignes(),this.nbLignes());
 		
-		 for(int i = 0; i<this.matrice.length;i++){
-			 for(int j = 0; j<this.matrice.length;j++){
-				mat.matrice[i][j] = this.matrice[j][i];
+		 // Intervertit les lignes et les colonnes
+		 for(int i = 0; i<this.nbLignes();i++){
+			 for(int j = 0; j<this.nbColonnes();j++){
+				mat.setElem(i,j,this.getElem(i,j));
 			 } 
 		 }
 		 return mat;
--- tme1/src/pc/Repeat.java
+++ tme1/src/pc/Repeat.java
@@ -3,12 +3,12 @@ package pc;
 public class Repeat {
 
 	public static String repeat(char c, int n) {
-		StringBuilder s = new StringBuilder();
+		StringBuilder s = new StringBuilder(); // Modif du type en StringBuilder
 		for (int i = 0; i < n; i++) {
 			s.append(c);
 		}
 		
-		return s.toString();
+		return s.toString(); // Convertit le StringBuilder en String avec la methode toString()
 	}
 		
 }
