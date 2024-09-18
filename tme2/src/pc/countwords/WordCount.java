765d10273ee62913bfa91b86330c5f7349463aef
rasheequa <rasheequa.m@ugmail.com>
2024-09-18 20:26:07 +0200

    rendu initial tme2

--- tme2/src/pc/countwords/WordCount.java
+++ tme2/src/pc/countwords/WordCount.java
@@ -5,31 +5,64 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class WordCount {
+public class WordCount extends Thread {
+	private String fichier;
+	private int [] tableau;
+	private int id;
 
+   
+    public WordCount(String arg,int [] tab, int id) {
+    	this.fichier=arg;
+    	this.tableau=tab;
+    	this.id=id;
+    }
+    
+    public void run(){
+        try {
+        	int count=countWords(fichier);
+        	tableau[id]=count;
+			System.out.println("---------------Thread "+id+"---------------\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+	
 	public static int countWords(String filename) throws IOException {
 		long startTime = System.currentTimeMillis();
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-			int total = 0;
+			int total = 0; 
 			for (String line = br.readLine(); line != null; line = br.readLine()) {
 				total += line.split("\\s+").length;
 			}
-			System.out.println("Time for file "+filename+" : "+(System.currentTimeMillis()-startTime) + " ms for "+ total + " words");
+			System.out.println("Time for file "+filename+" : "+(System.currentTimeMillis()-startTime) + " ms for "+ total + " words \n");
 			return total;
+			
 		}
 	}
 	
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		int [] wordCount = new int[args.length];
+		
+		Thread [] th = new Thread[args.length];
+		
 		for (int i = 0; i < args.length; i++) {
-			try {
-				wordCount[i] = countWords(args[i]);
-			} catch (IOException e) {
-				System.err.println("Error reading file: " + args[i]);
-				e.printStackTrace();
-			}
+			th[i]= new WordCount(args[i],wordCount,i);
+			th[i].start();
+
 		}
+		
+		for (int j = 0; j < args.length; j++) {
+			try {
+				th[j].join();
+				
+			} catch (InterruptedException e) {
+		        e.printStackTrace();
+		    }
+		}
+		
+		System.out.println("\n---------------------------------------\n");
+		
 		System.out.println("Word count:" + Arrays.toString(wordCount));
 		int total = 0;
 		for (int count : wordCount) {
